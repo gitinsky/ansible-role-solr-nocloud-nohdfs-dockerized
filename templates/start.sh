10d513a14ffaa4c4bde7a23d68ef9bf50269f361
Alex Chistyakov <alexclear@gmail.com>
2015-06-15 19:42:01 +0300

    Proper files

--- templates/start.sh
+++ templates/start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-export JAVA_HOME=/jdk1.8.0_45
-/solr/bin/solr -f -cloud -z {{ zookeeper_quorum }} -a "-Dsolr.directoryFactory=HdfsDirectoryFactory -Dsolr.lock.type=hdfs -Dsolr.hdfs.home=hdfs://192.168.160.122:8020/solrcloud"
+/solr/bin/solr -f
